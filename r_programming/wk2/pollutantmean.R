96e2c799fe843358b6a397053295c5e210c41051
Stephen Adams <sa597@kent.ac.uk>
2016-05-20 01:02:34 +0200

    Fixed week2 problem one

--- r_programming/wk2/pollutantmean.R
+++ r_programming/wk2/pollutantmean.R
@@ -2,9 +2,9 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   res <- c()
   for(i in id){
     fileName <- paste(directory, "/", padNumber(toString(i)),".csv",sep='')
-    table<- read.csv(fileName,header=TRUE)
-    table <- na.omit(table)
-    res <- c(res,table[,pollutant])
+    table<- read.csv(fileName,header=TRUE,sep=",")
+    cleaned <- table[!is.na(table[,pollutant]),pollutant]
+    res <- c(res,cleaned)
   }
   mean(res)
 }
@@ -16,3 +16,7 @@ padNumber <- function(num){
     num
   }
 }
+
+pollutantmean("specdata", "sulfate", 1:10)
+pollutantmean("specdata", "nitrate", 70:72)
+pollutantmean("specdata", "nitrate", 23)
